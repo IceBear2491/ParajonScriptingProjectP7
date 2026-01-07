72a7681e43e44f445b8064da161c3ffe77d85a6f
IceBear2491 <salvadorparajon5@gmail.com>
2026-01-07 14:17:46 -0800

    The cube says awake and start
    u9o8ygutfrdedsdcsddf cgiksdjnm cvzNXBCkq23ewr7yuijhbnd xcmz

---
+++ P7ParajonScriptingProject/Assets/Scripts/AwakeAndStart.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeAndStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Debug.Log("Awake Called");
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+        Debug.Log("Start Called");
+    }
+
+}
