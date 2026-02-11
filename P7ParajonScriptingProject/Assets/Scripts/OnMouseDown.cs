1868bcc08f9849adb581f2ecb81618b93dc3c8a3
IceBear2491 <salvadorparajon5@gmail.com>
2026-02-11 14:45:23 -0800

    siouth fbOREE THE FRACTED BUT WHOOLE
    QW

---
+++ P7ParajonScriptingProject/Assets/Scripts/OnMouseDown.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnMouseDown : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(-transform.forward * 500f);
+        rb.useGravity = true;
+    }
+}
