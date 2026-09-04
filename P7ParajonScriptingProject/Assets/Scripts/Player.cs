fac4984ab1664f31f147a10988f8ea23d9010303
IceBear2491 <salvadorparajon5@gmail.com>
2026-09-04 11:41:13 -0700

    woah I did a script
    yeah wassup

---
+++ P7ParajonScriptingProject/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    private int experience;
+
+    public int Experience
+    {
+        get 
+        {
+            return experience;
+        }
+        set
+        {
+            experience = value;  
+        }
+    }
+
+    public int Level
+    {
+        get
+        {
+            return experience / 1000;
+        }
+        set 
+        {
+            experience = value * 1000;
+        }
+    }
+    public int Health { get; set; }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
